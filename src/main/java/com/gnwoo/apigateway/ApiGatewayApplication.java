8e59faf570536b5246e2f7c0dd2a5a8f96ee0950
Xiaolei Luo <luo19980630@outlook.com>
2020-06-23 00:57:54 -0700

    Added initial skeleton code

---
+++ src/main/java/com/gnwoo/apigateway/ApiGatewayApplication.java
@@ -0,0 +1,22 @@
+package com.gnwoo.apigateway;
+
+import com.gnwoo.apigateway.Filters.pre.SimpleFilter;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+@EnableZuulProxy
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+	@Bean
+	public SimpleFilter simpleFilter() {
+		return new SimpleFilter();
+	}
+}
